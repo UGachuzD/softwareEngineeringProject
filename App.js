c70f6487266a54e5d12cb1793a9720a0bdb40210
UGachuzD <ulisesgachuz07@gmail.com>
2024-10-21 22:02:07 -0600

    Creacion proyecto

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NativeBaseProvider, Button, Box, Text, Center } from 'native-base';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Center flex={1}>
+        <Box>
+          <Text fontSize="lg" mb={4}>Prueba</Text>
+          <Button onPress={() => console.log("Sherman!")}>Peabody</Button>
+        </Box>
+      </Center>
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
